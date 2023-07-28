ad95499bb3850f013276e2019ba2aea068406856
XCSenior <2969271422@qq.com>
2023-07-28 14:55:10 +0800

    搭建框架

---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+const path = require('path')
+const webpack = require('webpack')
+const HtmlWebPackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'main.js'
+    },
+    devtool: 'eval-cheap-module-source-map',
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        port: 9000,
+        host: '0.0.0.0',
+        hot: true
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: [
+                    { loader: 'babel-loader', options: { presets: [['@babel/preset-env', { useBuiltIns: 'usage' }]] } }
+                ]
+            },
+            {
+                test: /\.css$/i,
+                exclude: /node_modules/,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.html$/,
+                loader: 'html-loader'
+            },
+        ]
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebPackPlugin({ template: './src/index.html' }),
+        new webpack.HotModuleReplacementPlugin(),
+        new CopyWebpackPlugin(
+            { patterns: [{ from: path.resolve(__dirname, 'public'), to: 'public' }] }
+        )
+    ]
+}

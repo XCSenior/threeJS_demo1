fd9ca09d729e66a2ea317c97f70389e720e3ed2a
XCSenior <2969271422@qq.com>
2023-07-28 18:24:21 +0800

    增加官方“小小东京”demo的引入

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
-// import "@/common/js/test1.js";
-// import "@/common/js/test2.js";
-import "@/common/js/load-model-test.js";
+// import "@/official-demo/demo1.js" // 官方案例1
+import "@/official-demo/little-tokyo.js" // 官方案例：小小东京
+// import "@/common/js/test1.js"; // 一个最简单的动画立方体
+// import "@/common/js/test2.js"; // 新增线
+// import "@/common/js/load-model-test.js"; // 加载模型测试

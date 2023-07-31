95a334bcacc168f044b53c6e2aee20e16e80c7a0
XCSenior <2969271422@qq.com>
2023-07-31 16:57:03 +0800

    复制小小东京案例作为基础

---
+++ src/common/js/little-tokyo2-CSS3DRender.js
@@ -0,0 +1,89 @@
+import * as THREE from "three";
+
+import Stats from "three/examples/jsm/libs/stats.module.js";
+
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+import { RoomEnvironment } from "three/examples/jsm/environments/RoomEnvironment.js";
+
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
+import { DRACOLoader } from "three/examples/jsm/loaders/DRACOLoader.js";
+
+let mixer;
+
+const clock = new THREE.Clock();
+const container = document.getElementById("container");
+
+const stats = new Stats();
+container.appendChild(stats.dom);
+
+const renderer = new THREE.WebGLRenderer({ antialias: true });
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth, window.innerHeight);
+container.appendChild(renderer.domElement);
+
+const pmremGenerator = new THREE.PMREMGenerator(renderer);
+
+const scene = new THREE.Scene();
+scene.background = new THREE.Color(0xbfe3dd);
+scene.environment = pmremGenerator.fromScene(
+    new RoomEnvironment(renderer),
+    0.04
+).texture;
+
+const camera = new THREE.PerspectiveCamera(
+    40,
+    window.innerWidth / window.innerHeight,
+    0.5,
+    100
+);
+camera.position.set(5, 2, 8);
+
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.target.set(0, 0.5, 0); // 设置了相机围绕其旋转的目标点
+controls.update(); // 更新控制器的内部状态，需要在任何修改之后调用
+controls.enablePan = false; // 禁用通过鼠标移动目标点
+controls.enableDamping = true; // 相机移动时产生平滑的效果
+
+const dracoLoader = new DRACOLoader();
+dracoLoader.setDecoderPath("/public/jsm/libs/draco/gltf/");
+
+const loader = new GLTFLoader();
+loader.setDRACOLoader(dracoLoader);
+loader.load("models/gltf/LittlestTokyo.glb", function (gltf) {
+        const model = gltf.scene;
+        model.position.set(1, 1, 0);
+        model.scale.set(0.01, 0.01, 0.01);
+        scene.add(model);
+
+        mixer = new THREE.AnimationMixer(model); // 播放模型动画
+        mixer.clipAction(gltf.animations[0]).play();
+
+        animate();
+    },
+    undefined,
+    function (e) {
+        console.error(e);
+    }
+);
+
+window.onresize = function () {
+    // 长宽比
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+
+    renderer.setSize(window.innerWidth, window.innerHeight);
+};
+
+function animate() {
+    requestAnimationFrame(animate);
+
+    const delta = clock.getDelta(); // 获取浏览器的刷新频率，设置动画刷新帧速率
+
+    mixer.update(delta);
+
+    controls.update();
+
+    stats.update();
+
+    renderer.render(scene, camera);
+}
--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 // import "@/official-demo/demo1.js" // 官方案例1
-import "@/official-demo/little-tokyo.js" // 官方案例：小小东京
+// import "@/official-demo/little-tokyo.js" // 官方案例：小小东京
 // import "@/common/js/test1.js"; // 一个最简单的动画立方体
 // import "@/common/js/test2.js"; // 新增线
 // import "@/common/js/load-model-test.js"; // 加载模型测试
+// import "@/common/js/dom-test.js"; // CSS3DRenderer测试
+import "@/common/js/little-tokyo2-CSS3DRender.js"; // CSS3DRenderer加在小小东京案例中测试
+
+
+
+
